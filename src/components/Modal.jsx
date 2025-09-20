608b3d2e278fd6a202002bfa584b009c8145770d
Federica Dincalci <federica.dincalci@gmail.com>
2025-09-20 15:30:48 +0200

    Final commit for complete project

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,7 +1,9 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
 import { createPortal } from "react-dom";
 
-const Modal = forwardRef(function Modal({children}, ref) {
+import Button from './Button';
+
+const Modal = forwardRef(function Modal({children, buttonCaption}, ref) {
     const dialog = useRef();
 
     useImperativeHandle(ref, () => {
@@ -13,8 +15,11 @@ const Modal = forwardRef(function Modal({children}, ref) {
     });
 
     return createPortal(
-        <dialog ref={dialog}>
+        <dialog ref={dialog} className="backdrop:bg-stone-900/90 p-4 rounded-md shadow-md">
             {children}
+            <form method="dialog" className="mt-4 text-right">
+                <Button>{buttonCaption}</Button>
+            </form>
         </dialog>, document.getElementById('modal-root')
     )
 });
